e825645ad7dc08c98d64d125d14003c027b1f248
atomlegacy1 <core85683@gmail.com>
2022-10-25 19:10:36 +0300

    Creating ActorWeaponClass and add some functions

--- Source/WeaponTest/Weapon/ActorWeaponClass.cpp
+++ Source/WeaponTest/Weapon/ActorWeaponClass.cpp
@@ -1,20 +1,49 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ActorWeaponClass.h"
+#include "DrawDebugHelpers.h"
 
-// Sets default values
+
 AActorWeaponClass::AActorWeaponClass()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	SceneRootComponent = CreateDefaultSubobject<USceneComponent>(FName("SceneRootComponent"));
+	SetRootComponent(SceneRootComponent);
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("WeaponMeshComponent"));
+	WeaponMesh->SetupAttachment(SceneRootComponent);
 
 }
 
-// Called when the game starts or when spawned
 void AActorWeaponClass::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
+void AActorWeaponClass::Shoot()
+{
+	if (isEmpty) return;
+
+	
+	ShootDelegate.Broadcast();
+	DrawDebugLine();
+	CurrentAmmo -= AmmoToLose ;
+}
+
+void AActorWeaponClass::Reloading()
+{
+	if (!isEmpty) return;
+
+	ReloadingDelegate.Broadcast();
+	CurrentAmmo = MaxAmmo;
+	
+}
+
+float AActorWeaponClass::GetCurrentAmmo()
+{
+	return CurrentAmmo;
+}
+
 
 
--- Source/WeaponTest/Weapon/ActorWeaponClass.h
+++ Source/WeaponTest/Weapon/ActorWeaponClass.h
@@ -6,11 +6,16 @@
 #include "GameFramework/Actor.h"
 #include "ActorWeaponClass.generated.h"
 
-UCLASS()
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FShootDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FReloadingDelegate);
+
+UCLASS(HideCategories=("Rendering","Replication","Collision","Input","Actor","LOD","Cooking"))
 class WEAPONTEST_API AActorWeaponClass : public AActor
 {
 	GENERATED_BODY()
 	
+#pragma region Default
+
 public:	
 	AActorWeaponClass();
 	
@@ -18,7 +23,51 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+#pragma endregion
 
+#pragma region Action
 
+public:
+	UFUNCTION(BlueprintCallable, Category = "Weapon Action")
+	void Shoot();
+	UFUNCTION(BlueprintCallable, Category = "Weapon Action")
+	void Reloading();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon Ammo Action")
+	float GetCurrentAmmo();
+
+#pragma endregion
+
+#pragma region Variables
+
+private:
+	UPROPERTY(meta=(ClampMin=0))
+	float CurrentAmmo{0};
+	UPROPERTY(meta=(ClampMax=20))
+	float MaxAmmo {20};
+	UPROPERTY();
+	float AmmoToLose{1};
+
+	UPROPERTY()
+	bool isEmpty{false};
+
+	UPROPERTY()
+	FShootDelegate ShootDelegate;
+
+	UPROPERTY()
+	FReloadingDelegate ReloadingDelegate;
+
+#pragma endregion
+
+#pragma region WeaponComponents
+
+	UPROPERTY()
+	USceneComponent* SceneRootComponent;
+
+	UPROPERTY(EditDefaultsOnly)
+	UStaticMeshComponent* WeaponMesh;
+
+#pragma endregion
+	
 
 };
